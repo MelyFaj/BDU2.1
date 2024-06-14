1158b9cdeaab079c4de761566622200bb30a28b9
MelyFaj <112254771+MelyFaj@users.noreply.github.com>
2024-06-14 08:34:02 +0600

    Add files via upload

---
+++ IRE 106 Electronics Devices and Applications Sessional/project/sketch_apr28a/sketch_apr28a.ino
@@ -0,0 +1,11 @@
+void setup() {
+ Serial.begin(9600);
+ String text1 = "This text, ";
+ String text2 = "has more words!";
+ String concatenatedText = text1 + text2;
+ int numberOfCharacters = 
+strchr('c', 1);
+ }
+ void loop() {
+ // Nothing to do in the loop
+ }
